3e97ceae8e3ed98e7d18f900c2c0f3ac0b406c3b
bogyeongs223 <bogyeongs223@gmail.com>
2021-12-31 19:04:51 +0900

    tailwind 추가

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./templates/**/*.{html,js}",
+    "./statics/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+// https://tailwindcss.com/docs/configuration
+// https://tailwindcss.com/docs/content-configuration
