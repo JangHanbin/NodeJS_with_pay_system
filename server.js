56fa045268ff0ab80a3209251de3793d13aaa37e
장한빈 <janghanbin@janghanbin-ui-MacBook-Pro.local>
2021-12-31 19:07:05 +0900

    update static folder name and server.js code

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const api = require('./router/api');
 
 app.use('/', router); // this line for main routing
 app.use('/api', api); // this line for api
+app.use(express.static('/static')) // to approachable static folder to link css, js files
 
 app.set('views', __dirname + '/templates');
 app.set('view engine', 'ejs');
+
 app.engine('html', require('ejs').renderFile);
 
 
